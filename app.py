98dfb8d4445c81e8f0e21bd80b3cc71617fed7d0
swapnil2830 <148048381+swapnil2830@users.noreply.github.com>
2025-01-27 15:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,9 +110,8 @@ frontend_settings = {
 # Enable Microsoft Defender for Cloud Integration
 MS_DEFENDER_ENABLED = os.environ.get("MS_DEFENDER_ENABLED", "true").lower() == "true"
 
-@bp.route("/webhook", methods=["GET", "POST"])
+@bp.route("/webhook", methods=["POST"])
 async def google_chat_webhook():
-    if request.method == "GET":
     return jsonify({
                 "text": "Hello"
             })
